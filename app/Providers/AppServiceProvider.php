0a6dc52c82b26e185942662b4c9501126ae52772
XTishka <takhir.berdyiev@gmail.com>
2024-05-05 20:24:20 +0300

    feat(base): Added language switcher

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['en','uk', 'ru'])
+                ->labels(['ua' => __('Ukraine')])
+                ->flags([
+                    'en' => asset('images/flags/us.svg'),
+                    'uk' => asset('images/flags/ua.svg'),
+                    'ru' => asset('images/flags/ru.svg'),
+                ]);
+        });
     }
 }
